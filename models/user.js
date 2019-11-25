fb56c5acc1db0c43cd5b4a2fe55412f4e050da3e
HuangMinShi <z7707092003@gmail.com>
2019-11-25 22:39:04 +0800

    Update models/user to dev branch

--- models/user.js
+++ models/user.js
@@ -1,6 +1,50 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {}, {});
-  User.associate = function(models) {};
+  const User = sequelize.define(
+    'User',
+    {
+      email: {
+        type: DataTypes.STRING,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING
+      },
+      name: {
+        type: DataTypes.STRING,
+        unique: true
+      },
+      avatar: {
+        type: DataTypes.STRING
+      },
+      introduction: {
+        type: DataTypes.TEXT
+      },
+      role: {
+        type: DataTypes.STRING
+      }
+    },
+    {}
+  );
+  User.associate = function (models) {
+    User.hasMany(models.Tweet);
+    User.hasMany(models.Reply);
+    User.hasMany(models.Like);
+    User.belongsToMany(models.Tweet, {
+      through: models.Like,
+      foreignKey: 'UserId',
+      as: 'LikedTweets'
+    });
+    User.belongsToMany(models.User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    });
+    User.belongsToMany(models.User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    });
+  };
   return User;
 };
