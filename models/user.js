bb990ca0794476110478be1897650f91c11383ff
HuangMinShi <z7707092003@gmail.com>
2019-11-25 20:46:35 +0800

    Update models/user

--- models/user.js
+++ models/user.js
@@ -1,6 +1,31 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {}, {});
-  User.associate = function(models) {};
+  const User = sequelize.define(
+    'User',
+    {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      name: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      introduction: DataTypes.TEXT,
+      role: DataTypes.STRING
+    },
+    {}
+  );
+  User.associate = function (models) {
+    User.hasMany(models.Tweet);
+    User.hasMany(models.Reply);
+    User.hasMany(models.Like);
+    User.belongsToMany(models.User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    });
+    User.belongsToMany(models.User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    });
+  };
   return User;
 };
