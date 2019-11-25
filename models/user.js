2fdabe276aac0939cb1c4ed8e268a871f9c24088
HuangMinShi <z7707092003@gmail.com>
2019-11-25 23:16:56 +0800

    Update models/user

--- models/user.js
+++ models/user.js
@@ -1,6 +1,42 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {}, {});
-  User.associate = function(models) {};
+  const User = sequelize.define(
+    'User',
+    {
+      email: {
+        type: DataTypes.STRING,
+        unique: true
+      },
+      password: DataTypes.STRING,
+      name: {
+        type: DataTypes.STRING,
+        unique: true
+      },
+      avatar: DataTypes.STRING,
+      introduction: DataTypes.TEXT,
+      role: DataTypes.STRING
+    },
+    {}
+  );
+  User.associate = function (models) {
+    User.hasMany(models.Tweet);
+    User.hasMany(models.Reply);
+    User.hasMany(models.Like);
+    User.belongsToMany(models.Tweet, {
+      through: models.Like,
+      foreignKey: 'UserId',
+      as: 'LikedTweets'
+    });
+    User.belongsToMany(models.User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    });
+    User.belongsToMany(models.User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    });
+  };
   return User;
 };
