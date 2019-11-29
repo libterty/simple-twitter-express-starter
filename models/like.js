827a0dc52cb78b142fa1956519d1dacc31ce4167
dreak603 <dreak603@gmail.com>
2019-11-29 15:33:14 +0800

    finish all the models and all pass UT

--- models/like.js
+++ models/like.js
@@ -1,6 +1,23 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Like = sequelize.define('Like', {}, {});
-  Like.associate = function(models) {};
+  // Sequelize issue Not returning id on create #7689
+  // Therefore adding autoIncrement on ID
+  const Like = sequelize.define(
+    'Like',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      UserId: DataTypes.INTEGER,
+      TweetId: DataTypes.INTEGER
+    },
+    {}
+  );
+  Like.associate = function(models) {
+    Like.belongsTo(models.Tweet);
+    Like.belongsTo(models.User);
+  };
   return Like;
 };
